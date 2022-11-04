677253fbb5267a3588f5c2817989c88f95589099
pm1v21 <p.morawski@soton.ac.uk>
2022-11-04 13:45:48 +0000

    Initial project structure

---
+++ src/assets/js/misc.js
@@ -0,0 +1,8 @@
+(function () {
+  // Add a body class once page has loaded
+  // Used to add CSS transitions to elems
+  // and avoids content shifting during page load
+  window.addEventListener('load', function () {
+    document.body.classList.add('page-loaded');
+  });
+})();
