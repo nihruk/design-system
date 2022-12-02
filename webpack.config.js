4055d508595b4074100589345fd14fa0d90f7067
pmor80 <40675513+pmor80@users.noreply.github.com>
2022-12-02 10:44:37 +0000

    Initial project structure with webpack (#5)
    Co-authored-by: pm1v21 <p.morawski@soton.ac.uk>

---
+++ webpack.config.js
@@ -0,0 +1,197 @@
+const path = require('path');
+var glob = require('glob-all');
+const webpack = require('webpack');
+const HandlebarsPlugin = require('handlebars-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+
+//PurgeCSS Paths
+const purgeCSSPaths = {
+  src: path.join(__dirname, 'src', 'html'),
+  partials: path.join(__dirname, 'src', 'partials')
+};
+
+// paths used in various placed in webpack config
+const paths = {
+  src: {
+    imgs: './src/assets/images',
+    scss: './src/assets/scss',
+    fonts: './src/assets/fonts',
+    js: './src/assets/js',
+    favicon: './src/assets/favicon'
+  },
+  dist: {
+    imgs: './assets/images',
+    css: './assets/css',
+    fonts: './assets/fonts',
+    js: './assets/js',
+    favicon: './assets/favicon'
+  }
+};
+
+// Main webpack config options.
+const wPackConfig = {
+  entry: {
+    libs: [paths.src.scss + '/libs.scss'],
+    theme: [paths.src.js + '/theme.js', paths.src.scss + '/theme.scss']
+  },
+  output: {
+    filename: paths.dist.js + '/[name].bundle.js'
+  },
+  devtool: 'source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss|css)$/,
+        include: path.resolve(__dirname, paths.src.scss.slice(2)),
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                indentWidth: 4,
+                outputStyle: 'expanded',
+                sourceComments: true
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/](node_modules)[\\/].+\.js$/,
+          name: 'vendor',
+          chunks: 'all'
+        }
+      }
+    },
+    minimizer: [
+      new OptimizeCssAssetsPlugin({
+        cssProcessorOptions: {
+          map: {
+            inline: false
+          }
+        },
+        cssProcessorPluginOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: {
+                removeAll: true
+              }
+            }
+          ]
+        }
+      }),
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: paths.src.fonts,
+          to: paths.dist.fonts,
+          noErrorOnMissing: true
+        },
+        {
+          from: paths.src.imgs,
+          to: paths.dist.imgs,
+          noErrorOnMissing: true
+        },
+        {
+          from: paths.src.favicon,
+          to: paths.dist.favicon,
+          noErrorOnMissing: true
+        }
+      ]
+    }),
+    new HandlebarsPlugin({
+      entry: path.join(process.cwd(), 'src', 'html', '**', '*.html'),
+      output: path.join(process.cwd(), 'dist', '[path]', '[name].html'),
+      partials: [path.join(process.cwd(), 'src', 'partials', '**', '*.{html,svg}')],
+      helpers: {
+        webRoot: function () {
+          return '{{webRoot}}';
+        },
+        config: function (data) {
+          return data;
+        },
+        ifEquals: function (arg1, arg2, options) {
+          if (arg1 === arg2) {
+            return options.fn(this);
+          }
+          return options.inverse(this);
+        },
+        log: function (data) {
+          console.log(data);
+        },
+        limit: function (arr, limit) {
+          if (!Array.isArray(arr)) {
+            return [];
+          }
+          return arr.slice(0, limit);
+        }
+      },
+      onBeforeSave: function (Handlebars, res, file) {
+        const elem = file.split('//').pop().split('/').length;
+        return res.split('{{webRoot}}').join('.'.repeat(elem));
+      }
+    }),
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: paths.dist.css + '/[name].bundle.css'
+    }),
+    new PurgeCSSPlugin({
+      paths: glob.sync([`${purgeCSSPaths.src}/**/*`, `${purgeCSSPaths.partials}/**/*`], {
+        nodir: true
+      }),
+      safelist: {
+        greedy: [
+          /show$/,
+          /collapsing$/,
+          /aos/,
+          /data/,
+          /reveal/,
+          /show-filters/,
+          /modal/,
+          /collapse/,
+          /slideout/
+        ]
+      }
+    })
+  ]
+};
+
+module.exports = wPackConfig;
