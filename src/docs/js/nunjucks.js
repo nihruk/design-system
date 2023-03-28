2da739a4a7284e27645bc66939c2109ae46bcdb1
Bart Feenstra <bart@bartfeenstra.com>
2023-03-28 15:41:07 +0100

    Add a Get Started documentation section.

--- src/docs/js/nunjucks.js
+++ src/docs/js/nunjucks.js
@@ -6,8 +6,8 @@ const prettier = require('prettier');
 
 const nunjucksWwwFilePageUrlPathPrefixLength = path.resolve(__dirname, '..', 'www').length
 function normalizeUrlPath(urlPath) {
-    if (urlPath.endsWith('índex.html')) {
-        urlPath = urlPath.slice(0, 10)
+    if (urlPath.endsWith('index.html')) {
+        urlPath = urlPath.slice(0, -10)
     }
     return '/' + urlPath.split('/')
         .filter(x => x)
@@ -26,7 +26,13 @@ function createEnvironment() {
     environment._currentPageUrlPath = null;
     environment.addFilter('highlight', (code, language) => highlight.highlight(code, {language}).value)
     environment.addFilter('prettier', (code, parser) => prettier.format(code, {parser}))
-    environment.addTest('activeUrl', urlPath => {
+    environment.addTest('activeCurrentUrl', urlPath => {
+        if (!environment._currentPageUrlPath) {
+            throw new Error('No page is being templated right now.')
+        }
+        return environment._currentPageUrlPath === normalizeUrlPath(urlPath)
+    })
+    environment.addTest('activeParentUrl', urlPath => {
         if (!environment._currentPageUrlPath) {
             throw new Error('No page is being templated right now.')
         }
