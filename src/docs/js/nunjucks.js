611eec199298b0a30fd9dfa8911b40ae8fcd9d19
Bart Feenstra <bart@bartfeenstra.com>
2023-04-23 10:32:07 +0100

    Improve the robustness of the macro documentation and its test.

--- src/docs/js/nunjucks.js
+++ src/docs/js/nunjucks.js
@@ -1,7 +1,15 @@
+'use strict'
+
 const nunjucks = require('nunjucks');
 const path = require('path');
 const highlight = require('highlight.js');
 const prettier = require('prettier');
+const fs = require('fs');
+const {lexer, parser} = require('nunjucks');
+const assert = require('assert');
+const {TOKEN_COMMENT} = require('nunjucks/src/lexer');
+const nodes = require('nunjucks/src/nodes');
+const commentParser = require('comment-parser');
 
 
 const nunjucksWwwFilePageUrlPathPrefixLength = path.resolve(__dirname, '..', 'www').length
@@ -16,12 +24,10 @@ function normalizeUrlPath(urlPath) {
 
 function createEnvironment() {
     const environment = new nunjucks.Environment(
-        new nunjucks.FileSystemLoader(
-            path.resolve(__dirname, '..', '..'),
-            {
-                throwOnUndefined: true,
-            },
-        )
+        new nunjucks.FileSystemLoader(path.resolve(__dirname, '..', '..')),
+        {
+            throwOnUndefined: true,
+        },
     )
     environment._currentPageUrlPath = null;
     environment.addFilter('highlight', (code, language) => highlight.highlight(code, {language}).value)
@@ -41,7 +47,7 @@ function createEnvironment() {
     return environment
 }
 
-function renderNunjucksFile(filename) {
+function renderFile(filename) {
     const environment = createEnvironment()
     environment._currentPageUrlPath = normalizeUrlPath(
         filename.slice(nunjucksWwwFilePageUrlPathPrefixLength)
@@ -53,6 +59,107 @@ function renderNunjucksFile(filename) {
     })
 }
 
+function* getNodesByType(node, type) {
+    if (node === undefined) {
+        return
+    }
+
+    if (node instanceof type) {
+        yield node
+    }
+
+    if (node.body !== undefined) {
+        yield* getNodesByType(node.body, type)
+    }
+
+    if (node.children !== undefined) {
+        for (const child of node.children) {
+            yield* getNodesByType(child, type)
+        }
+    }
+
+    if (node.cond !== undefined) {
+        yield* getNodesByType(node.cond.args, type)
+        yield* getNodesByType(node.cond.left, type)
+        yield* getNodesByType(node.cond.right, type)
+    }
+
+    if (node.arr !== undefined) {
+        yield* getNodesByType(node.arr, type)
+    }
+
+    if (node.args !== undefined) {
+        yield* getNodesByType(node.args, type)
+    }
+}
+
+function nunjucksJsDocCommentToJsDoc(nunjucksComment) {
+    const commentText = nunjucksComment.value.slice(2, -2).trim()
+    const jsDocComment = '/**\n'.concat(
+        commentText
+            .split('\n')
+            .map(line => ' * ' + line)
+            .join('\n'),
+        '\n */',
+    )
+    return commentParser.parse(jsDocComment)[0]
+}
+
+function getMacroForFileContents(filePath, fileContents) {
+    const ast = parser.parse(fileContents)
+    const macros = []
+    for (const macro of getNodesByType(ast, nodes.Macro)) {
+        if (macro.name.value.startsWith('_')) {
+            continue
+        }
+        macros.push(macro)
+    }
+    assert(macros.length === 1, `The file ${filePath} must contain exactly one public macro, but ${Object.keys(macros).length} were found.`)
+    return macros[0]
+}
+
+function getMacroJsDocForFileContents(filePath, fileContents) {
+    const tokens = lexer.lex(fileContents)
+    let token
+    while ((token = tokens.nextToken()) !== null) {
+        if (token.type !== TOKEN_COMMENT) {
+            continue
+        }
+        const jsDoc = nunjucksJsDocCommentToJsDoc(token)
+        const macroTags = jsDoc.tags.filter(tag => tag.tag === 'macro')
+        assert(macroTags.length <= 1, `The file ${filePath} contains a docblock with more than one @macro tag.`)
+        if (macroTags.length) {
+            return [macroTags[0].name, jsDoc]
+        }
+    }
+    throw new Error(`The file ${filePath} must contain exactly one JSDoc docblock for a public macro, but none were found.`)
+}
+
+function getMacroJsDocForFilePath(filePath) {
+    const fileContents = fs.readFileSync(filePath, {
+        encoding: 'utf-8',
+    })
+
+    const macro = getMacroForFileContents(filePath, fileContents)
+    const [jsDocMacroName, jsDoc] = getMacroJsDocForFileContents(filePath, fileContents)
+    assert(macro.name.value === jsDocMacroName, `The file ${filePath} contains a macro ${macro.name.value}(), but has a "@macro ${jsDocMacroName}" docblock.`)
+
+    const macroOptionNames = [...new Set(Array.from(getNodesByType(macro, nodes.LookupVal))
+        .filter(node => node.target.value === 'options')
+        .map(node => node.val.value))]
+    const jsDocOptionNames = jsDoc.tags
+        .filter(tag => tag.tag === 'param')
+        .map(tag => tag.name)
+
+    const undocumentedCommentOptionNames = jsDocOptionNames.filter(name => !macroOptionNames.includes(name))
+    const undocumentedMacroOptionNames = macroOptionNames.filter(name => !jsDocOptionNames.includes(name))
+    assert(undocumentedCommentOptionNames.length === 0, `The file ${filePath} documents the following options that are not used by the macro in the same file: ${undocumentedCommentOptionNames.join(', ')}`)
+    assert(undocumentedMacroOptionNames.length === 0, `The file ${filePath} uses the following options that are not documented in the same file: ${undocumentedMacroOptionNames.map(name => 'options.' + name).join(', ')}`)
+
+    return jsDoc
+}
+
 module.exports = {
-    renderNunjucksFile,
+    renderFile,
+    getMacroJsDocForFilePath,
 }
--- tests/macro-documentation.js
+++ tests/macro-documentation.js
@@ -1,77 +1,9 @@
-const fs = require('fs');
-const {globSync} = require('glob');
-const {lexer, parser} = require('nunjucks');
-const assert = require('assert');
-const {TOKEN_COMMENT, TOKEN_BLOCK_START} = require('nunjucks/src/lexer');
-const path = require('path');
-const {LookupVal} = require('nunjucks/src/nodes');
-const { parse } = require('comment-parser')
+'use strict'
 
-function* getNodesByType(node, type) {
-    if (node === undefined) {
-        return
-    }
+const {globSync} = require('glob')
+const {getMacroJsDocForFilePath} = require("../src/docs/js/nunjucks")
 
-    if (node instanceof type) {
-        yield node
-    }
-
-    if (node.body !== undefined) {
-        yield* getNodesByType(node.body, type)
-    }
-
-    if (node.children !== undefined) {
-        for (const child of node.children) {
-            yield* getNodesByType(child, type)
-        }
-    }
-
-    if (node.cond !== undefined) {
-        yield* getNodesByType(node.cond.args, type)
-        yield* getNodesByType(node.cond.left, type)
-        yield* getNodesByType(node.cond.right, type)
-    }
-}
-
-function validateMacro(filePath) {
-    const fileContents = fs.readFileSync(filePath, {
-        encoding: 'utf-8',
-    })
-
-    const tokens = lexer.lex(fileContents)
-    const firstToken = tokens.nextToken()
-    assert([TOKEN_COMMENT, TOKEN_BLOCK_START].includes(firstToken.type), `The file ${filePath} MUST start its first line with a code comment or the macro itself. Instead, a "${firstToken.type}" was found.`)
-    const comment = firstToken.type === TOKEN_COMMENT ? firstToken : null;
-
-    const ast = parser.parse(fileContents)
-    const macro = ast.children[comment ? 1 : 0]
-    const macroName = macro.name.value
-    const fileName = path.basename(filePath, '.html')
-    assert(macroName === fileName, `The file ${filePath} MUST contain a single macro named "${fileName}". Instead, a macro named "${macroName}" was found.`)
-    assert(
-        macro.args.children.length === 1 && macro.args.children[0].value === 'args',
-        `The macro "${macroName}" in ${filePath} MUST take a single argument named "args".`,
-    )
-    const macroArgNames = Array.from(getNodesByType(ast, LookupVal))
-        .filter(node => node.target.value === 'args')
-        .map(node => node.val.value)
-
-    if (macroArgNames.length > 0) {
-        assert(comment !== null, `The file ${filePath} MUST contain a comment on its first line, documenting the "args." variables used in the macro.`)
-    }
-    if (comment) {
-        const commentText = comment.value.slice(2, -2).trim()
-        const jsDocComment = '/**\n'.concat(commentText.split('\n').map(line => ' * ' + line).join('\n'),  '\n */')
-        const parsedComment = parse(jsDocComment)[0]
-        const commentArgNames = parsedComment.tags
-            .filter(comment => comment.tag === 'param')
-            .map(comment => comment.name)
-        const undocumentedCommentArgNames = commentArgNames.filter(name => !macroArgNames.includes(name))
-        const undocumentedMacroArgNames = macroArgNames.filter(name => !commentArgNames.includes(name))
-        assert(undocumentedCommentArgNames.length === 0, `The file ${filePath} documents the following arguments that are not used by the macro in the same file: ${undocumentedCommentArgNames.join(', ')}`)
-        assert(undocumentedMacroArgNames.length === 0, `The file ${filePath} uses the following arguments that are not documented in the same file: ${undocumentedMacroArgNames.map(name => 'args.' + name).join(', ')}`)
-    }
-}
-
-globSync([__dirname, '..', 'src', 'macros', '**', '*.html'].join('/'))
-    .forEach(validateMacro)
+globSync([
+    [__dirname, '..', 'src', 'macros', '**', '[!_]*.html'].join('/'),
+    [__dirname, '..', 'src', 'docs', 'macros', '**', '[!_]*.html'].join('/'),
+]).forEach(getMacroJsDocForFilePath)
